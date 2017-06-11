1f017ab2bb1305e95c6aa889dd38ba0523aeb4fd
ScalaWilliam <william@scalawilliam.com>
2017-06-11 12:19:56 +0800

    Odd.

--- ansible/prod-deploy.sh
+++ ansible/prod-deploy.sh
@@ -6,6 +6,7 @@
 git pull && \
     sbt "show universal:packageZipTarball" && \
     cp /home/build/git-work/web/target/universal/git-work-0.1-SNAPSHOT.tgz ~/www/ && \
+    cd ansible && \
     time ansible-playbook \
         -i git.work, \
         --extra-vars "web_hostname=git.work" \
